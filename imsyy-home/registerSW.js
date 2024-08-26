94b1e83ce31dc1b2d3d3d6866ec3903c9258fe34
onlinewwj <jiissb@126.com>
2024-08-26 10:27:36 +0800

    Upload home.

---
+++ imsyy-home/registerSW.js
@@ -0,0 +1,11 @@
+if (/*@cc_on!@*/ false || (!!window.MSInputMethodContext && !!document.documentMode)){window.location.href = 'https://ssle.cn/check-browser-ie.html';}
+if (!localStorage.getItem('token')) {
+  document.onkeydown = function () {
+    if (window.event && window.event.keyCode == 123) {
+        event.keyCode = 0;
+        event.returnValue = false;
+    }
+  }
+  ConsoleBan.init();
+}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
